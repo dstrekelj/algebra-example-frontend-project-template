baf72e33d1f297d35ac7ae281a05c6d64802b615
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-03-22 20:05:45 +0100

    Extract CSS file in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isProduction = process.env.NODE_ENV === "production";
 const inputPath = path.join(__dirname, "src", "index.js");
 const outputPath = path.join(__dirname, "build");
 const publicPath = path.join(__dirname, "public");
-
 module.exports = {
   entry: inputPath,
   output: {
@@ -26,11 +26,12 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [isProduction ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(publicPath, "index.html"),
     }),
